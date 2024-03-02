ce431efd4163ace7756a328aec87841be59b9ff0
kon <156806318+kon65138@users.noreply.github.com>
2024-03-02 01:55:59 +0000

    add resizable grid button (limit to 100)

--- javascript.js
+++ javascript.js
@@ -1,10 +1,35 @@
 let grids = [];
-const container = document.querySelector('.container')
-
-for (let i = 0; i < 256; i++)  {
+let gridno = 256;
+let container = document.querySelector('.container')
+const btn = document.querySelector('button');
+btn.addEventListener("click", chooseGridSize)
+function chooseGridSize () {
+    for (let grid of grids) grid.remove();
+    let sides = prompt("how many squares per side?");
+    if (sides > 100) sides = 100;
+    gridno = sides * sides;
+    for (let i = 0; i < gridno; i++)  {
+        grids[i] = document.createElement('div');
+        grids[i].classList.add(`grid${i}`);
+        let percent = 100/sides;
+        container.appendChild(grids[i]);
+        grids[i].style.cssText = `width:${percent}%; height:${percent}%;`;
+    }
+    for (let grid of grids) {
+        function colorize () {
+            grid.style.backgroundColor = 'black';
+            setTimeout( () => {
+                grid.style.backgroundColor = 'transparent';
+            }, 300)
+        }
+        grid.addEventListener("mouseenter", colorize);
+    }
+}
+for (let i = 0; i < gridno; i++)  {
     grids[i] = document.createElement('div');
-    grids[i].classList.add(`grid${i}`);
+    grids[i].classList.add(`grid`);
     container.appendChild(grids[i]);
+    grids[i].style.cssText = `width:6.25%; height:6.25%;`;
 }
 
 for (let grid of grids) {
@@ -12,7 +37,8 @@ for (let grid of grids) {
         grid.style.backgroundColor = 'black';
         setTimeout( () => {
             grid.style.backgroundColor = 'transparent';
-        }, 300)
+        }, 500)
     }
     grid.addEventListener("mouseenter", colorize);
 }
+
