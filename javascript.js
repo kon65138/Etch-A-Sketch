433d78fcd51f5e743a2c7051b5bcfee74f83d94b
kon <156806318+kon65138@users.noreply.github.com>
2024-03-02 00:15:21 +0000

    add hover trails on grid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
-let grid = [];
+let grids = [];
 const container = document.querySelector('.container')
 
 for (let i = 0; i < 256; i++)  {
-    grid[i] = document.createElement('div');
-    grid[i].classList.add(`grid${i}`);
-    container.appendChild(grid[i]);
+    grids[i] = document.createElement('div');
+    grids[i].classList.add(`grid${i}`);
+    container.appendChild(grids[i]);
+}
+
+for (let grid of grids) {
+    function colorize () {
+        grid.style.backgroundColor = 'black';
+        setTimeout( () => {
+            grid.style.backgroundColor = 'transparent';
+        }, 300)
+    }
+    grid.addEventListener("mouseenter", colorize);
 }
